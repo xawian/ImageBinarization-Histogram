99ef3d2d5a7f32a710cf5d84d6e02b403c7671b2
xawian <66998201+xawian@users.noreply.github.com>
2023-03-02 18:19:44 +0100

    GUI

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
+import PySimpleGUI as sg
+from io import BytesIO
 
 
 def binarization_average(image, treshold):
+    pix = image.load()
     for x in range(image.width):
         for y in range(image.height):
             averageRGB = (pix[x, y][1] + pix[x, y][2] + pix[x, y][2]) / 3
@@ -11,36 +14,52 @@ def binarization_average(image, treshold):
                 pix[x, y] = (255, 255, 255)
             else:
                 pix[x, y] = (0, 0, 0)
+    bio = BytesIO()
+    image.save(bio, format='PNG')
+    window['IMAGE'].update(data=bio.getvalue())
 
 
 def binarization_R(image, treshold):
+    pix = image.load()
     for x in range(image.width):
         for y in range(image.height):
             if pix[x,y][0] > treshold:
                 pix[x, y] = (255, 255, 255)
             else:
                 pix[x, y] = (0, 0, 0)
+    bio = BytesIO()
+    image.save(bio, format='PNG')
+    window['IMAGE'].update(data=bio.getvalue())
 
 
 def binarization_G(image, treshold):
+    pix = image.load()
     for x in range(image.width):
         for y in range(image.height):
             if pix[x,y][1] > treshold:
                 pix[x, y] = (255, 255, 255)
             else:
                 pix[x, y] = (0, 0, 0)
+    bio = BytesIO()
+    image.save(bio, format='PNG')
+    window['IMAGE'].update(data=bio.getvalue())
 
 
 def binarization_B(image, treshold):
+    pix = image.load()
     for x in range(image.width):
         for y in range(image.height):
             if pix[x,y][2] > treshold:
                 pix[x, y] = (255, 255, 255)
             else:
                 pix[x, y] = (0, 0, 0)
+    bio = BytesIO()
+    image.save(bio, format = 'PNG')
+    window['IMAGE'].update(data = bio.getvalue())
 
 
 def generate_histogram_R(image):
+    pix = image.load()
     histogram = np.zeros([256])
     for x in range(image.width):
         for y in range(image.height):
@@ -55,6 +74,7 @@ def generate_histogram_R(image):
 
 
 def generate_histogram_G(image):
+    pix = image.load()
     histogram = np.zeros([256])
     for x in range(image.width):
         for y in range(image.height):
@@ -69,6 +89,7 @@ def generate_histogram_G(image):
 
 
 def generate_histogram_B(image):
+    pix = image.load()
     histogram = np.zeros([256])
     for x in range(image.width):
         for y in range(image.height):
@@ -83,6 +104,7 @@ def generate_histogram_B(image):
 
 
 def generate_histogram_average(image):
+    pix = image.load()
     histogram = np.zeros([256])
     for x in range(image.width):
         for y in range(image.height):
@@ -95,12 +117,48 @@ def generate_histogram_average(image):
     plt.xlabel("Pixel Value")
     plt.show()
 
+# pix = im.load()
+# binarization_average(im,128)
+# generate_histogram_average(im)
+# im.show()
 
-im = Image.open("samples/baboon.jpg")
-pix = im.load()
-binarization_average(im,128)
-generate_histogram_average(im)
-im.show()
+control_gui = sg.Column([
+    [sg.Frame('Treshold', layout = [[sg.Slider(range = (0, 255), orientation = 'h', key = 'TRESH')]])],
+    [sg.Checkbox('R', key = '-R-', enable_events=True), sg.Checkbox('G', key = '-G-', enable_events=True), sg.Checkbox('B', key = '-B-', enable_events=True),
+     sg.Checkbox('Average', key = '-AVG-', enable_events=True)],
+    [sg.Button('Binarization', key = '-BINARIZATION-'), sg.Button('Histogram', key = '-HISTOGRAM-')],
+    [sg.Button('Save image', key = '-SAVE-'), sg.Button('Upload image', key = '-UPLOAD-'), sg.Button('Reset', key = 'RESET')],
+])
+
+image_gui = sg.Column([[sg.Image('samples/lenna.png', key = 'IMAGE')]])
+layout = [[control_gui, image_gui]]
+
+original = Image.open('samples/lenna.png')
+window = sg.Window('Biometrics', layout)
+while True:
+    event, values = window.read(timeout = 50)
+    if event in ['-R-', '-G-', '-B-', '-AVG-']: #only one checkbox
+        for key in ['-R-', '-G-', '-B-', '-AVG-']:
+            if key != event:
+                window[key].update(False)
+    if event == sg.WIN_CLOSED:
+        break
+    if event == '-BINARIZATION-' and values['-B-'] == True:
+        binarization_B(original, values['TRESH'])
+    if event == '-BINARIZATION-' and values['-R-'] == True:
+        binarization_R(original, values['TRESH'])
+    if event == '-BINARIZATION-' and values['-G-'] == True:
+        binarization_G(original, values['TRESH'])
+    if event == '-BINARIZATION-' and values['-AVG-'] == True:
+        binarization_average(original, values['TRESH'])
+    if event == '-HISTOGRAM-' and values['-AVG-'] == True:
+        generate_histogram_average(original)
+    if event == 'RESET':
+        original = Image.open('samples/lenna.png')
+        bio = BytesIO()
+        original.save(bio, format='PNG')
+        window['IMAGE'].update(data=bio.getvalue())
+window.close()
 
 
 
